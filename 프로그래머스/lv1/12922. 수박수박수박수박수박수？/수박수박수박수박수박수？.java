5877a1f6ac4edc5a28dee89a633bfabe57fe1fd7
Junghun Sim <96688359+092600@users.noreply.github.com>
2023-04-10 00:49:21 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 1.99 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -1,11 +1,9 @@
 class Solution {
     public String solution(int n) {
-        String[] tmp = {"수", "박"};
-        
         if (n % 2 == 0) {
-            return (tmp[0] + tmp[1]).repeat(n / 2);
+            return ("수박").repeat(n / 2);
         } else {
-            return (tmp[0] + tmp[1]).repeat(n / 2) + tmp[0];
+            return ("수박").repeat(n / 2) + "수";
         }
     }
 }
